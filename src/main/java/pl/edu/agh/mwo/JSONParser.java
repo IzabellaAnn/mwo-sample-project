7f52e563c1320509f6b4a172f46457c0acbd8995
student <krysz@agh.edu.pl>
2024-05-25 15:37:25 +0200

    #9 - empty classes

---
+++ src/main/java/pl/edu/agh/mwo/JSONParser.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.mwo;
+
+public class JSONParser {
+    public static class AveragePitStopTime {
+    }
+
+    public static class FirstAndLastPitStop {
+    }
+}
---
+++ src/main/java/pl/edu/agh/mwo/RaceData.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.mwo;
+
+public class RaceData {
+
+    int pitDuration;
+    int lapNumber;
+    int driverNumber;
+
+}
